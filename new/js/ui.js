1ba46e206df26914ff222f39937bff80c74a3bf3
user-PC\user <damanegi1979@naver.com>
2016-11-24 18:00:58 +0900

    no message

--- new/js/ui.js
+++ new/js/ui.js
@@ -1,7 +1,6 @@
 /*
  * Plugin intialization
  */
-
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
         menu: '#menu',
@@ -73,12 +72,17 @@ $(document).ready(function() {
         var logo = new TimelineLite();
         var tit = $(".intro_contents");
         var sub_tit = $(".sub_title");
-        logo.to(tit, 1.5,{marginTop:0, opacity:1})
-        logo.to(tit, 3.5,{transform:'perspective(1000px) rotateX(20deg) rotateY(20deg)'},3)
+        logo.to(tit, 1.0,{marginTop:0, opacity:1})
+        logo.to(tit, 2.5,{transform:'perspective(1000px) rotateX(20deg) rotateY(-20deg)'},1)
+        logo.to(tit, 2.5,{transform:'perspective(1000px) rotateX(20deg) rotateY(20deg)'},3.5)
         logo.to( tit, 1.0, {transform:'perspective(1000px) rotateX(0deg) rotateY(0deg) '})
-        logo.from(sub_tit, 0.5, {opacity:0,transform:'perspective(1000px) rotateX(0deg) translateY(30px)',onComplete:axis})
-        logoText();
+        logo.from(sub_tit, 0.5, {opacity:0,transform:'perspective(1000px) rotateX(0deg) translateY(30px)',onComplete:function(){
+                                        axis()
+                                        scrollfn()
+                                        $("#svg_logo").addClass('logoChg')
+                                    }
 
+                                })
     }
     function logoText() {
         var split = new TimelineLite;
@@ -91,21 +95,40 @@ $(document).ready(function() {
         TweenMax.to( tit, 2.5, {transform:'perspective(1000px) rotateX(15deg) rotateY(-20deg)'} );
     }
     function axis(){
-        $(".section").delay(1000).bind("mouseover", function (){
+
+        $(".section").delay(500).bind("mouseover", function (){
             $(".intro_contents").bind("mousemove", moveLogo);
         });
     }
-    function moveLogo( e )
+    function scrollfn(){
+        var scrollDown = $(".scrollDown")
+        var wheel = $(".scrollDown .ico_scroll");
+        scrollDown.fadeIn(function(){
+            TweenMax.to( wheel, 1, {transform: 'translateY(20px)',repeat:-1, yoyo:true} );
+        });
+    }
+    function moveLogo(e)
     {
         var pageX = e.pageX - $(".section").offset().left;
         var pageY = e.pageY - $(".section").offset().top;
         pageX = pageX - ($(".section").width()/2);
-        pageY = pageY - ($(".section").height()/2)-scrollY;
+        pageY = pageY - ($(".section").height()/2); //-scrollY
         var percentX = pageX / ($(".section").width()/2);
         var percentY = pageY / ($(".section").height()/2);
         var rotationY = -15*percentX;
         var rotationX = 15*percentY;
-        TweenMax.to($(".intro_contents"), 0.6, {rotationY:rotationY, rotationX:rotationX,x:rotationY, y:-rotationX});
+        TweenMax.to($(".intro_contents"), 0.6, {rotationY:rotationY, rotationX:rotationX,x:rotationY*2, y:-rotationX*2});
 
     }
+    new Vivus('svg_logo',{duration: 200,type:'delayed'}, function (obj) {
+        obj.el.classList.add('finished');
+    });
+    setTimeout(function(){
+        new Vivus('svg_text', {duration: 200,type:'delayed'}, function (obj) {
+
+        })
+    },2000)
+
+
 });
+
