307fce025697bda820f4e9e4cba5da99dee5c337
user-PC\user <damanegi1979@naver.com>
2016-12-08 17:43:38 +0900

    no message

--- new/js/ui_service.js
+++ new/js/ui_service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-$(document).ready(function() {
+$(window).load(function() {
     $("body").niceScroll({
         cursorcolor: "rgba(0,0,0,0.5)", // change cursor color in hex
         cursoropacitymin: 1, // change opacity when cursor is inactive (scrollabar "hidden" state), range from 1 to 0
@@ -14,15 +14,16 @@ $(document).ready(function() {
     });
     scroll3d()
     headerMotion()
-//    axisPortfolio()
+    service()
+    axisCard()
 })
 
 function headerMotion(){
     var controller = new ScrollMagic.Controller();
     var header = $('#transition_wrapper .header .contents')
-    var headerTween = TweenMax.to(header, 2, {transform:'translateY(50vh) scale(0.7)',opacity:0.5})
-    var header_scene = new ScrollMagic.Scene({triggerElement:this, offset:0, triggerHook: 'onLeave', duration:'100%'}).setTween(headerTween)
+    var headerTween = TweenMax.to(header, 2, {transform:'translateY(30vh) scale(0.8)',opacity:0.5})
 
+    var header_scene = new ScrollMagic.Scene({triggerElement:this, offset:0, triggerHook: 'onLeave', duration:'100%'}).setTween(headerTween)
 
     controller.addScene([
         header_scene
@@ -80,24 +81,22 @@ function scroll3d(){
         ]);
     })
 }
-function axisPortfolio(){
-    $('.itemList').each(function(){
+function axisCard(){
+    $('.cardArea').each(function(){
         var thisList = this;
-        $(this).bind("mouseover",function(){
-            console.log('in')
-            if($(this).hasClass('active')){
+        $(this).bind("mouseenter",function(){
                 $(this).bind("mousemove",moveFolio)
-
-            }
+                TweenMax.to($(this).find('.card'), 0.5, {transform: "scale(1.1) translateY(-20px)",x:0, y:0});
+                TweenMax.to($(this).find('.shadow'), 0.5, {transform: "scale(0.8) translateY(40px)",x:0, y:0});
+                console.log('on')
         })
         $(this).bind("mouseleave",function(){
-            if($(this).hasClass('active')){
-                TweenMax.to($(this).find('[data-role="moveTarget-1"]'), 0.5, {transform: "translateZ(0)",x:0, y:0});
-                TweenMax.to($(this).find('[data-role="moveTarget-2"]'), 0.5, {transform: "translateZ(0)",x:0, y:0});
-                TweenMax.to($(this).find('[data-role="moveTarget-3"]'), 0.5, {transform: "translateZ(0)",x:0, y:0});
+//                TweenMax.to($(this).find('[data-role="moveTarget-1"]'), 0.5, {transform: "translateZ(0)",x:0, y:0});
+//                TweenMax.to($(this).find('[data-role="moveTarget-2"]'), 0.5, {transform: "translateZ(0)",x:0, y:0});
+            TweenMax.to($(this).find('.card'), 0.5, {transform: "scale(1) translateY(0px)",x:0, y:0});
+            TweenMax.to($(this).find('.shadow'), 0.5, {transform: "scale(1) translateY(0px)",x:0, y:0});
                 console.log('out')
                 $(this).unbind("mousemove",moveFolio)
-            }
         })
     })
 
@@ -115,3 +114,26 @@ function moveFolio(e){
     TweenMax.to($(this).find('[data-role="moveTarget-2"]'), 0.2, {x:xx*2,y:yy*1.5});
     TweenMax.to($(this).find('[data-role="moveTarget-3"]'), 0.2, {x:xx*4,y:yy*2.5});
 }
+
+
+function service(){
+    var controller = new ScrollMagic.Controller();
+    $('.service_container').each(function(){
+        var thisList = this;
+        var itemTxt = $(thisList).find('.text_area b')
+        var itemTxtarea = $(thisList).find('.text_area')
+//        var durationArea = $(thisList).find('.text_area')
+
+        var txtTween = TweenMax.to(itemTxt, 2, {color:'#128dd4'})
+        var areaTween = TweenMax.to(itemTxtarea, 2, {transform:"translateX(0%)",opacity:1})
+
+        var txt_scene = new ScrollMagic.Scene({triggerElement:this, offset:50, duration:100}).setTween(txtTween)
+        var area_scene = new ScrollMagic.Scene({triggerElement:this, offset:100, triggerHook: 'onEnter', duration:400}).setTween(areaTween)
+
+
+        controller.addScene([
+            txt_scene,area_scene
+        ]);
+
+    })
+ }
