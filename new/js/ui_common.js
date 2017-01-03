f4243e20a769199131f3c69637a37aab393ff171
조재영 <damanegi1979@naver.com>
2017-01-03 21:25:28 +0900

    no message

--- new/js/ui_common.js
+++ new/js/ui_common.js
@@ -34,12 +34,12 @@
                 $("#ajaxContainer").addClass('fixed')
                 TweenMax.to(outPage, 0.6, {x:-($(window).width()*0.5), force3D:true,ease:Power2.easeIn, zIndex:1, onComplete:motionEnd});
                 $("#ajaxContainer").append(page);
-                
+
                 TweenMax.to($("#ajaxContainer>#ajaxContents").eq(1), 0, {x:($(window).width()),zIndex:10});
 //                $("#ajaxContents").scrollTop(0)
                 TweenMax.to($("#ajaxContainer>#ajaxContents").eq(1), 0.6, {x:0, force3D:true,ease:Power2.easeIn});
                 $('.menuOpen .stats').text(url)
-                
+
                 function motionEnd()
                 {
                     if(url != "home") nest[url].init();
@@ -47,7 +47,7 @@
                     outPage.remove();
                     oldPage = currentPage;
                     if(nest[oldUrl]) nest[oldUrl].dispos();
-                    $("#ajaxContainer").removeClass('fixed')   
+                    $("#ajaxContainer").removeClass('fixed')
 
                 }
                 if(url=="home") setTimeout(function (){nest[url].init();}, 100);
@@ -99,8 +99,8 @@
     })();
 
     nest.service = (function (){
-        
-        
+
+
         return {
             init : function (){
 //                scroll()
@@ -152,6 +152,7 @@
         return {
             init : function (){
                 $("#ajaxContainer").addClass('fixed')
+                copyToClipboard(element)
                 console.log('contact class start')
             },
             dispos : function ()
@@ -224,4 +225,13 @@ function menu(){
             TweenMax.to(content,0,({transform:'translateX(-100vw)',force3D:true}))
         }}))
     })
+}
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    alert('이메일 주소')
+    $temp.remove();
 }
