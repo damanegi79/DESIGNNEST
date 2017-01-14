c0c4ed43a7d8d08213640e4afb301c6c30fd6e97
조재영 <damanegi1979@naver.com>
2017-01-14 18:43:29 +0900

    no message

--- new/sendmail.php
+++ new/sendmail.php
@@ -1,20 +1,20 @@
 <?php
     include "./include/Sendmail.php";
-    
-    /* 
-    * host    : smtp 호스트 주소 
+
+    /*
+    * host    : smtp 호스트 주소
     * smtp_id : smtp 계정 아이디
     * smtp_pw : smtp 계정 비번
     * debug   : 디버그표시기능 [1 : 활성 0 : 비활성]
     * charset : 문자 인코딩
-    * ctype   : 메일 컨텐츠의 타입 
+    * ctype   : 메일 컨텐츠의 타입
     */
     $config = array(
-        'host' => 'ssl://smtp.gmail.com',
-        'smtp_id'=>'hohomkd@gmail.com', 
-        'smtp_pw'=>'ansruden1', 
-        'debug'=>1, 
-        'charset'=>'utf-8', 
+        'host' => 'ssl://smtp.daum.net',
+        'smtp_id'=>'designnest',
+        'smtp_pw'=>'poi612lkj',
+        'debug'=>1,
+        'charset'=>'utf-8',
         'ctype'=>'text/plain'
     );
 
@@ -27,12 +27,12 @@
     * $body : 메일 내용
     * $cc_mail : Cc 메일 있을경우 (옵션값으로 생략가능)
     * $bcc_mail : Bcc 메일이 있을경우 (옵션값으로 생략가능)
-    */ 
-    $to="hohomkd@gmail.com"; 
-    $from="고객"; 
-    $subject="프로젝트 의뢰"; 
-    $body="메일 내용입니다."; 
-    $cc_mail=""; 
+    */
+    $to="designnest@daum.net";
+    $from="고객";
+    $subject="프로젝트 의뢰";
+    $body="메일 내용입니다.";
+    $cc_mail="";
     $bcc_mail="";
 
     $sendmail->send_mail($to, $from, $subject, $body);
