c58252222f11717dab510b6d1b2d8267cb524f4e
kyung-doo <hohomkd@naver.com>
2017-01-14 20:59:02 +0900

    no message

--- new/sendmail.php
+++ new/sendmail.php
@@ -1,39 +1,60 @@
-<?php
-    include "./include/Sendmail.php";
-    
-    /* 
-    * host    : smtp 호스트 주소 
-    * smtp_id : smtp 계정 아이디
-    * smtp_pw : smtp 계정 비번
-    * debug   : 디버그표시기능 [1 : 활성 0 : 비활성]
-    * charset : 문자 인코딩
-    * ctype   : 메일 컨텐츠의 타입 
-    */
-    $config = array(
-        'host' => 'ssl://smtp.gmail.com',
-        'smtp_id'=>'hohomkd@gmail.com', 
-        'smtp_pw'=>'ansruden1', 
-        'debug'=>1, 
-        'charset'=>'utf-8', 
-        'ctype'=>'text/plain'
-    );
+<?
+//보내는 이
+  $recipient = "damanegi1979@naver.com";
+//제목 처리
+  $subject = '=?UTF-8?B?'.base64_encode($_GET['title']).'?=';
+//메일주소
+  $mail_from = '=?UTF-8?B?'.base64_encode($_GET['email']).'?=';
 
-    $sendmail = new Sendmail($config);
+//메일내용
+  $mail_body = "<table width='600' border='0' cellpadding='0' cellspacing='1' bgcolor='#CCCCCC'>
+        <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>제목</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['title']."</td></tr>".
 
-    /*
-    * $to : 받는사람 메일주소 ( ex. $to="hong <hgd@example.com>" 으로도 가능)
-    * $from : 보내는사람 이름
-    * $subject : 메일 제목
-    * $body : 메일 내용
-    * $cc_mail : Cc 메일 있을경우 (옵션값으로 생략가능)
-    * $bcc_mail : Bcc 메일이 있을경우 (옵션값으로 생략가능)
-    */ 
-    $to="hohomkd@gmail.com"; 
-    $from="고객"; 
-    $subject="프로젝트 의뢰"; 
-    $body="메일 내용입니다."; 
-    $cc_mail=""; 
-    $bcc_mail="";
+        " <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>업체명</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['comuser']."</td></tr>".
 
-    $sendmail->send_mail($to, $from, $subject, $body);
+       " <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>성함</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['senduser']."</td></tr>".		
+		  
+       " <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>연락처</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['phone']."</td></tr>".	
+		  
+       " <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>이메일</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['email']."</td></tr>".	
+ 
+  	   " <tr> 
+          <td width='100' height='30' align='center' bgcolor='#eeeeee'>내용</td>
+          <td width='400' bgcolor='#FFFFFF'>". $_GET['body']."</td></tr>".	
+		  
+      "</table>";
+
+ 
+//메일 발송처리
+
+  $header = "From:$subject\n";
+  $header = "Content-Type: text/html;charset=utf-8";
+  $header .= "From : $mail_from <".$mail_from.">\n"; 
+
+ 
+  $email = mail($recipient, $subject, $mail_body, $header);
+
+
+
+  if (!$email)
+    echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /> <script>
+         window.alert('메일 발송이 실패하였습니다. ');
+         history.go(-1);
+         </script>";
+  else
+    echo "         <script>
+         window.alert('메일이 정상적으로 발송되었습니다.');
+         history.go(-1);
+         </script>";
 ?>
+
