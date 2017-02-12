4db1925f25476c063c1a0c68f45602faa407d6d0
unknown <>
2017-02-12 15:11:23 +0900

    no message

---
+++ giftCard/js/ui.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+//var viewPortWidth = $(window).width();
+//if (viewPortWidth > 320){//아이패드 사이즈일때
+//    document.getElementsByTagName("meta")[1].content="user-scalable=no, width=90%, initial-scale=1.1, maximum-scale=1.1, minimum-scale=1.1";
+//}else if (viewPortWidth > 767){
+//    document.getElementsByTagName("meta")[1].content="user-scalable=no, width=50%, initial-scale=2, maximum-scale=2, minimum-scale=2";
+//}
+
+
+$(document).ready(function(){
+
+})
+
+
+
+$(window).load(function () {
+
+});
+
